4aa95c29719b0fed455eff67f3ae9f819da585e9
bright772 <bright772@naver.com>
2026-01-17 23:49:55 +0900

    Home page updates (date)

--- scripts/dates.js
+++ scripts/dates.js
@@ -1,4 +1,5 @@
 // support dates
 const thisYear = new Date().getFullYear();
-const lastModified = new Date(document.last)
+const lastModified = new Date(document.lastModified).toDateString();
 document.getElementById("year-copy").innerText = thisYear;
+document.getElementById("date-modify").innerText = lastModified;
