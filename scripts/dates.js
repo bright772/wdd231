99d4bea0cba95f9208ff01a220034ae289f2c6e5
bright772 <bright772@naver.com>
2026-01-17 23:46:43 +0900

    Home page updates

--- scripts/dates.js
+++ scripts/dates.js
@@ -1,2 +1,4 @@
 // support dates
-
+const thisYear = new Date().getFullYear();
+const lastModified = new Date(document.last)
+document.getElementById("year-copy").innerText = thisYear;
--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,11 @@
 // support responsive navigation
 const menuButton = document.getElementById("menu-button");
 const navDrop = document.querySelector("nav");
+const navHolder = document.getElementById("nav-holder-div");
 
 menuButton.addEventListener("click", function () {
     menuButton.classList.toggle("show");
     navDrop.classList.toggle("show");
+    navHolder.classList.toggle("show");
 })
 
