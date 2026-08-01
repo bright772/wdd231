cb6bac9c599905219f1bb5b18c80a642af0e7c38
bright772 <90653984+bright772@users.noreply.github.com>
2026-08-01 16:31:10 +0900

    Implement error checking for navigation elements
    Added error checking for menu button and navigation dropdown.

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,10 +1,27 @@
 // support responsive navigation
 const menuButton = document.getElementById("menu-button");
 const navDrop = document.querySelector("nav");
-const navHolder = document.getElementById("nav-holder-div");
 
-menuButton.addEventListener("click", function () {
-    menuButton.classList.toggle("show");
-    navDrop.classList.toggle("show");
-    navHolder.classList.toggle("show");
-})
+// this 'if' is a form of error checking
+if (menuButton) {
+    if (navDrop) { // can do && here
+        menuButton.addEventListener("click", function () {
+            menuButton.classList.toggle("show");
+            navDrop.classList.toggle("show");
+            }
+        )
+    }
+}
+
+// This could be a way to check the current page:
+// document.querySelectorAll(".nav-links").forEach((link) => {
+//     const linkPage = link.getAttribute("href");
+//     const herePage = location.pathname.split("/").pop() || "index.html";
+//     link.classList.remove("current-page", "non-current-page");
+//     if (linkPage === herePage) {
+//         link.classList.add("current-page");
+//         link.setAttribute("aria-current", "page");
+//     } else {
+//         link.classList.add("non-current-page");
+//     }
+// });
